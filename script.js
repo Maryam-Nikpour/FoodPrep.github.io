e9f50b732f10be6dfd23862c05911d9ca662cb9f
Maryam-Nikpour <109689812+Maryam-Nikpour@users.noreply.github.com>
2023-12-12 08:55:56 -0500

    Add blurring background

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const mobilebtn=document.querySelector('.mobile-btn');
 const navMenu=document.querySelector('.menu');
 const navItem=document.querySelectorAll('.nav-item');
-const panels=document.querySelectorAll('.panel');
+
 mobilebtn.addEventListener('click',()=>{
   
     const isOpened=mobilebtn.getAttribute('aria-expanded');
@@ -19,6 +19,9 @@ navItem.forEach(navItem => {
     });
 });
 
+// Sliding recipes
+
+const panels=document.querySelectorAll('.panel');
 panels.forEach((panel)=>{
     panel.addEventListener('click',() => {
        removeActiveClasses()
@@ -33,3 +36,27 @@ panels.forEach((panel)=>{
    
        })
    }
+
+// Blurring background
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('header')
+
+let load = 0
+
+let int = setInterval(blurring, 25)
+
+function blurring() {
+  load++
+
+  if (load > 99) {
+    clearInterval(int)
+  }
+
+  loadText.innerText = `${load}%`
+  loadText.style.opacity = scale(load, 0, 100, 1, 0)
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`
+
+}
+const scale = (num, in_min, in_max, out_min, out_max) => {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+  }
